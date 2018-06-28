d1362f4aeafc779f0e5d62fd18422a90fa36604b
Pablo Marrero <=>
2018-06-28 14:43:03 -0300

    Modify structure in app.js to support react-router-dom

--- src/App.js
+++ src/App.js
@@ -2,32 +2,15 @@ import React, { Component } from 'react';
 //import logo from './logo.svg';
 import './App.css';
 
-import AppBar from '@material-ui/core/AppBar';
-import Toolbar from '@material-ui/core/Toolbar';
-import Typography from '@material-ui/core/Typography';
-
-import IconButton from '@material-ui/core/IconButton';
-import MenuIcon from '@material-ui/icons/Menu';
-import { BusquezaCerveza } from './componentes/busqueda-cerveza/busquedaCerveza';
+import { CervezaRoutes } from './routes'
+import { BrowserRouter } from 'react-router-dom'
 
 class App extends Component {
   render() {
     return (
-      <div>
-        <div className="main-cerveza">
-          <AppBar position="static">
-            <Toolbar>
-              <IconButton className="" color="inherit" aria-label="Menu">
-                <MenuIcon />
-              </IconButton>
-              <Typography variant="title" color="inherit" className={"cerveceria-title"}>
-                La Cerveceria Piola
-              </Typography>
-            </Toolbar>
-          </AppBar>
-          <BusquezaCerveza/>
-        </div>
-      </div>
+      <BrowserRouter>
+        <CervezaRoutes/>
+      </BrowserRouter>
     );
   }
 }
