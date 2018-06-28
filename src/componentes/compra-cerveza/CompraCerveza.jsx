e021f13ed7bf4e12fe4fd1c0950ebc5d4c20031c
Pablo Marrero <=>
2018-06-28 14:44:26 -0300

    Added button to navigate between views

--- src/componentes/compra-cerveza/CompraCerveza.jsx
+++ src/componentes/compra-cerveza/CompraCerveza.jsx
@@ -1,4 +1,6 @@
 import React, { Component } from 'react'
+import Button from '@material-ui/core/Button'
+import './CompraCerveza.css'
 
 export class CompraCerveza extends Component {
 
@@ -6,6 +8,7 @@ export class CompraCerveza extends Component {
         return (
             <div className="compra-cerveza" >
                 <h1>Compra</h1>
+                <Button onClick={()=>this.props.history.push('/')}>Volverrrr</Button>
             </div>
         )
     }
