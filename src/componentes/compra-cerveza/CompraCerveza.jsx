10a0a90208bef49031bec280afc356d1f959266c
Pablo Marrero <=>
2018-06-28 14:06:07 -0300

    Basic template to the compraCerveza component

---
+++ src/componentes/compra-cerveza/CompraCerveza.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+
+export class CompraCerveza extends Component {
+
+    render() {
+        return (
+            <div className="compra-cerveza" >
+                <h1>Compra</h1>
+            </div>
+        )
+    }
+}
