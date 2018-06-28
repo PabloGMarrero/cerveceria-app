b826c31a106a5e9fb7cc371ee9415f04304dddae
Pablo Marrero <=>
2018-06-28 14:42:18 -0300

    Added new route to cervezadetalle

--- src/routes.js
+++ src/routes.js
@@ -1,13 +1,15 @@
 import React from 'react'
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
-import CervezaDetalle from './componentes/cerveza-detalle/CervezaDetalle'
-import CompraCerveza from './componentes/compra-cerveza/CompraCerveza'
+import { CerveceriaMain } from './componentes/cerveceria-main/CerveceriaMain'
+import { CervezaDetalle } from './componentes/cerveza-detalle/CervezaDetalle'
+import { CompraCerveza } from './componentes/compra-cerveza/CompraCerveza'
 
 export const CervezaRoutes = () => (
     <Router>
         <Switch>
-            <Route exact path="/" component={CervezaDetalle}/>
-            <Route exact path="/compra" component ={CompraCerveza}/>
+            <Route exact path="/" component={CerveceriaMain} />
+            <Route exact path="/detalle" component={CervezaDetalle} />
+            <Route exact path="/compra" component={CompraCerveza} />
         </Switch>
     </Router>
 )
